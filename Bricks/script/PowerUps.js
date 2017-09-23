89e57bca3b8bc462bcf76472380e0b261b3d6bc8
lukas.lebo <lukas.lebo@hotmail.com>
2017-09-23 19:26:37 +0200

    new powerup: multi ball

--- Bricks/script/PowerUps.js
+++ Bricks/script/PowerUps.js
@@ -7,9 +7,9 @@ function PowerUp(x,y) {
     this.y = y;
     this.R = 5;
     this.fallVel = 1;
+    this.type = Math.floor(Math.random()*2);
     this.isCollected = false;
     this.failed = false;
-
 }
 
 PowerUp.prototype.draw = function() {
@@ -17,9 +17,20 @@ PowerUp.prototype.draw = function() {
         return;
     }
     this.ctx.beginPath();
-    this.ctx.fillStyle = 'red';
+    if (this.type==0) {
+        this.ctx.fillStyle = 'red';
+    }
+    else if (this.type==1) {
+        this.ctx.fillStyle = 'yellow';
+    }
     this.ctx.arc(this.x,this.y,this.R,0,Math.PI*2);
     this.ctx.fill();
+    this.ctx.beginPath();
+    this.ctx.arc(this.x,this.y,this.R*0.8,0,Math.PI*2);
+    this.ctx.arc(this.x,this.y,this.R*0.4,0,Math.PI*2);
+    this.ctx.lineWidth = 1;
+    this.ctx.strokeStyle = 'black';
+    this.ctx.stroke();
 }
 
 PowerUp.prototype.move = function() {
@@ -27,7 +38,7 @@ PowerUp.prototype.move = function() {
 }
 
 PowerUp.prototype.check = function(pX,pY,pW,pH,pOff) {
-    if (this.failed) {
+    if (this.failed || this.isCollected) {
         return false;
     }
     if (
