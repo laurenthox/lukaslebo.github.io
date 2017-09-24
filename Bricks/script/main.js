9756e119516c6a51885935eba696b7d2de3d2c61
lukas.lebo <lukas.lebo@hotmail.com>
2017-09-24 22:03:16 +0200

    warn safari users

--- Bricks/script/main.js
+++ Bricks/script/main.js
@@ -5,5 +5,6 @@ function closure() {
 }
 
 var myClosure = new closure();
-
-
+var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
+var is_safari = navigator.userAgent.indexOf("Safari") > -1;
+if (is_safari && !is_chrome) {alert("Sounds may not play correctly in Safari. Please try using Chrome.");}
