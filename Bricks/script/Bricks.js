d98a780f963f78acf82d0ae9dde1f9bc7edeef9f
lukas.lebo <lukas.lebo@hotmail.com>
2017-09-24 20:02:12 +0200

    brick visuals

--- Bricks/script/Bricks.js
+++ Bricks/script/Bricks.js
@@ -6,7 +6,10 @@ function Brick(x,y,lifes) {
     this.posX = x;
     this.posY = y;
     this.isHit = false;
-    this.color = ['#0004ff','#e500ff','#ff7b00'];
+    // this.color = ['#0004ff','#e500ff','#ff7b00'];
+    this.color = [['#000b66','#0008ff','#0061ff','#008cff','#96cfff'],
+                    ['#8e1818','#ff4c00','#ff7200','#ffa100','#ffc159'],
+                    ['#4a0059','#760c8c','#a50b93','#ff00e1','#ff93f2']];
     this.lifes = (lifes>1 && lifes<=3 && typeof(lifes)=='number')? lifes:1;
 }
 
@@ -14,8 +17,26 @@ Brick.prototype.draw = function() {
     if (this.isHit) {
         return;
     }
-    this.ctx.fillStyle = this.color[this.lifes-1];
-    this.ctx.fillRect(this.posX,this.posY,this.brickW,this.brickH);
+    var x = this.posX;
+    var y = this.posY;
+    var dx = this.brickW/16;
+    var dy = this.brickH/8;
+    var c = this.lifes-1;
+    this.ctx.fillStyle = this.color[c][0];
+    this.ctx.fillRect(x,y,16*dx,8*dy);
+    this.ctx.beginPath();
+    this.ctx.fillStyle = this.color[c][2];
+    this.ctx.fillRect(x,y,8*dx,1*dy);
+    this.ctx.fillStyle = this.color[c][3];
+    this.ctx.fillRect(x+8*dx,y,4*dx,1*dy);
+    this.ctx.fillStyle = this.color[c][4];
+    this.ctx.fillRect(x+12*dx,y,4*dx,7*dy);
+    this.ctx.fillStyle = this.color[c][1];
+    this.ctx.fillRect(x+1*dx,y+1*dy,14*dx,6*dy);
+    this.ctx.fillRect(x+15*dx,y+7*dy,1*dx,1*dy);
+    this.ctx.fillStyle = this.color[c][0];
+    this.ctx.fillRect(x+2*dx,y+4*dy,3*dx,1*dy);
+    this.ctx.fillRect(x+12*dx,y+3*dy,2*dx,1*dy);
 }
 
 Brick.prototype.check = function(bX, bY, bR, velX, velY,ballOnFire) {
